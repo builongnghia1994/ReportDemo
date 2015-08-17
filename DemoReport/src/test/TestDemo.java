d9f087cbf525ffa0915ee3655d4b13c3ed3c8cc3
builongnghia1994 <builongnghia1994@gmail.com>
2015-08-17 13:44:16 +0700

    update main method

--- DemoReport/src/test/TestDemo.java
+++ DemoReport/src/test/TestDemo.java
@@ -5,6 +5,8 @@ import student.StudentInfor;
 public class TestDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		
+		StudentInfor st = new StudentInfor("Nghia", 21);
+		System.out.println("My name is "+st.getName());
+		System.out.println("I am "+st.getAge());
 	}
 }
