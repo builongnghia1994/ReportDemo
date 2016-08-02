be1cc6044a15221205beaa595d4b69a46bf462e6
Nghia Bui <nghia.bui@logigear.com>
2016-08-02 16:45:12 +0700

    Add a new test project

---
+++ DemoGit/DemoGit/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoGit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.Write("Toi la ai");
+        }
+    }
+}
